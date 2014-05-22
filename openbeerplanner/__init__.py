eff34289b57a05a0ea6b0180919f69ed98b09c9b
kinou <kinou@rilynett.com>
2014-05-22 17:39:49 +0200

    fix regression

--- openbeerplanner/__init__.py
+++ openbeerplanner/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_restful import Api
 from flask_heroku import Heroku
 from os import environ
 import logging
@@ -12,8 +11,6 @@ app.debug = True# if environ.get('DEBUG') in ['True', 'true', '1'] else False
 heroku = Heroku(app)
 #app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 
-api = Api(app)
-
 #with the deveopement server we nned to serve the index.html
 
 @app.route('/')
@@ -22,16 +19,16 @@ def root():
 
 @app.route('/list')
 def list():
-    amenities = ws.get_amenity(ws.Coord(48.84680, 2.37628))
+    amenities = ws.get_amenities(ws.Coord(48.84680, 2.37628))
     amenities = ws.sort_and_filter(amenities)
     logging.debug(len(amenities))
     return render_template('list.html', amenities=amenities)
     #return app.send_static_file('index.html')
-	
-	
+
+
 @app.route('/fdr/<type>')
 def fdr(type):
-    amenities = ws.get_amenity(ws.Coord(48.84680, 2.37628))
+    amenities = ws.get_amenities(ws.Coord(48.84680, 2.37628))
     amenities = ws.sort_and_filter(amenities)
     logging.debug(len(amenities))
     return render_template('fdr.html', amenities=amenities[type])
