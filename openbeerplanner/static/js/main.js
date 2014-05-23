726219d28bc1cd848462ddb2bc05b8ffe2d18587
Charles Beaute <cjfbeaute@gmail.com>
2014-05-23 05:22:49 +0200

    homepage stylée

--- openbeerplanner/static/js/main.js
+++ openbeerplanner/static/js/main.js
@@ -7,10 +7,9 @@ $(document).ready(function() {
   });
   
   
-  today=new Date();
-
-  hours= today.getHours();  
-  dateheure = today.getHours() + ":" + today.getMinutes();today.getHours() + ":" + today.getMinutes();
+today=new Date();
+	today.setTime(today.getTime()+120*60*1000);
+	dateheure = today.getHours() + ":" + today.getMinutes();
   document.getElementById("time").innerHTML=dateheure;
 });
 
@@ -21,6 +20,36 @@ function setURL()
 	value2 = document.getElementById("rangeSuccessOutput").value;
 	result = "list?time="+value1+"&humor="+value2;
 	document.getElementById("button").setAttribute("href", result);
+}
+
+function setTime(){
+	value = document.getElementById("rangePrimary").value;
+	
+	if (value == "1")
+	{
+		result = 60;
+	}
+	if (value == "2")
+	{
+		result = 90;
+	}
+	if(value == "3")
+	{
+		result = 120;
+	}
+	if(value == "4")
+	{
+		result = 150;
+	}
+	if(value == "5")
+	{
+		result = 180;
+	}
+	today=new Date();
+	today.setTime(today.getTime()+result*60*1000);
+	dateheure = today.getHours() + ":" + today.getMinutes();
+  document.getElementById("time").innerHTML=dateheure;
+	
 }
 
 function hourgetter() {
@@ -50,6 +79,7 @@ function hourgetter() {
 	document.getElementById("hour").innerHTML=result;
 	document.getElementById("rangePrimaryOutput").value=value;
 	setURL();
+	setTime();
 }
 
 function shoesgetter() {
@@ -68,7 +98,6 @@ function shoesgetter() {
 	{
 		result = "rando";
 	}
-	document.getElementById("shoes").innerHTML=result;
 	document.getElementById("rangeSuccessOutput").value=value;
 	setURL();
 }
