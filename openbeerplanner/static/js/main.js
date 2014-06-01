90bad6716fb9307c5cd7b63142cb375c74ba6162
kinou <kinou@rilynett.com>
2014-06-01 18:11:37 +0200

    retab all template and removing of trailling space

--- openbeerplanner/static/js/main.js
+++ openbeerplanner/static/js/main.js
@@ -3,95 +3,95 @@ $(document).ready(function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
-	interval: 0,
+    interval: 0,
   });
 });
 
 
 function setURL()
 {
-	value1 = document.getElementById("rangePrimaryOutput").value;
-	value2 = document.getElementById("rangeSuccessOutput").value;
-	result = "list?time="+value1+"&humor="+value2;
-	document.getElementById("button").setAttribute("href", result);
+    value1 = document.getElementById("rangePrimaryOutput").value;
+    value2 = document.getElementById("rangeSuccessOutput").value;
+    result = "list?time="+value1+"&humor="+value2;
+    document.getElementById("button").setAttribute("href", result);
 }
 
 function setTime(){
-	value = document.getElementById("rangePrimary").value;
-	
-	if (value == "1")
-	{
-		result = 60;
-	}
-	if (value == "2")
-	{
-		result = 90;
-	}
-	if(value == "3")
-	{
-		result = 120;
-	}
-	if(value == "4")
-	{
-		result = 150;
-	}
-	if(value == "5")
-	{
-		result = 180;
-	}
-	today=new Date();
-	today.setTime(today.getTime()+result*60*1000);
-	dateheure = today.getHours() + ":" + today.getMinutes();
+    value = document.getElementById("rangePrimary").value;
+    
+    if (value == "1")
+    {
+        result = 60;
+    }
+    if (value == "2")
+    {
+        result = 90;
+    }
+    if(value == "3")
+    {
+        result = 120;
+    }
+    if(value == "4")
+    {
+        result = 150;
+    }
+    if(value == "5")
+    {
+        result = 180;
+    }
+    today=new Date();
+    today.setTime(today.getTime()+result*60*1000);
+    dateheure = today.getHours() + ":" + today.getMinutes();
   document.getElementById("time").innerHTML=dateheure;
-	
+
 }
 
 function hourgetter() {
 
-	value = document.getElementById("rangePrimary").value;
-	
-	if (value == "1")
-	{
-		result = "1h00";
-	}
-	if (value == "2")
-	{
-		result = "1h30";
-	}
-	if(value == "3")
-	{
-		result = "2h00";
-	}
-	if(value == "4")
-	{
-		result = "2h30";
-	}
-	if(value == "5")
-	{
-		result = "3h00";
-	}
-	document.getElementById("hour").innerHTML=result;
-	document.getElementById("rangePrimaryOutput").value=value;
-	setURL();
-	setTime();
+    value = document.getElementById("rangePrimary").value;
+
+    if (value == "1")
+    {
+        result = "1h00";
+    }
+    if (value == "2")
+    {
+        result = "1h30";
+    }
+    if(value == "3")
+    {
+        result = "2h00";
+    }
+    if(value == "4")
+    {
+        result = "2h30";
+    }
+    if(value == "5")
+    {
+        result = "3h00";
+    }
+    document.getElementById("hour").innerHTML=result;
+    document.getElementById("rangePrimaryOutput").value=value;
+    setURL();
+    setTime();
 }
 
 function shoesgetter() {
 
-	value = document.getElementById("rangeSuccess").value;
-	
-	if (value == "1")
-	{
-		result = "pantoufle";
-	}
-	if (value == "2")
-	{
-		result = "basket";
-	}
-	if(value == "3")
-	{
-		result = "rando";
-	}
-	document.getElementById("rangeSuccessOutput").value=value;
-	setURL();
+    value = document.getElementById("rangeSuccess").value;
+
+    if (value == "1")
+    {
+        result = "pantoufle";
+    }
+    if (value == "2")
+    {
+        result = "basket";
+    }
+    if(value == "3")
+    {
+        result = "rando";
+    }
+    document.getElementById("rangeSuccessOutput").value=value;
+    setURL();
 }
